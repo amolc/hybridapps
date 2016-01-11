14e1cd08ee52c69640730b328eaea93a904148bc
sameervedpathak <sameer.vedpathak18@gmail.com>
2016-01-11 16:58:27 +0530

    change in device_register api

--- templates/api/device_register.js
+++ templates/api/device_register.js
@@ -6,29 +6,46 @@ var connection = env.Dbconnection;
 var devicecrud = CRUD(connection,'device_information');
 
 exports.deviceregister = function(req,res){
-  	  devicecrud.create({
-  	  	'platform':req.body.platform,
-  	  	'userid':req.body.userid,
-  	  	'device_token':req.body.device_token,
-  	  	'deviceid':req.body.deviceid,
-  	  	'created_on':env.timestamp(),
-  	  	'modified_on':env.timestamp()
-  	  },function(error, result) {
-	    if (result) {
-	      responsedata = {
-	        status: true,
-	        record: result,
-	        registration_id: result.insertId,
-	        message: 'Device Register successfully'
-	      }
-	      res.jsonp(responsedata);
-	    } else {
-	      responsedata = {
-	        status: false,
-	        record: result,
-	        message: 'Device Register Failed'
-	      }
-	      res.jsonp(responsedata);
-	    }
-  });
+	  devicecrud.load({
+	  	'deviceid':req.body.deviceid,
+	  },function (err,val){
+	  	    var responsedata={
+            record:'',
+            status:false,
+            message :'err'
+        };
+        if(val.length>0){
+	        responsedata.record=val;
+	        responsedata.status=false;
+	        responsedata.message='Device Already Register';
+	        res.jsonp(responsedata);
+	      }else{
+	      	 devicecrud.create({
+		  	  	'platform':req.body.platform,
+		  	  	'userid':req.body.userid,
+		  	  	'device_token':req.body.device_token,
+		  	  	'deviceid':req.body.deviceid,
+		  	  	'created_on':env.timestamp(),
+		  	  	'modified_on':env.timestamp()
+		  	  },function(error, result) {
+			    if (result) {
+			      responsedata = {
+			        status: true,
+			        record: result,
+			        registration_id: result.insertId,
+			        message: 'Device Register successfully'
+			      }
+			      res.jsonp(responsedata);
+			    } else {
+			      responsedata = {
+			        status: false,
+			        record: result,
+			        message: 'Device Register Failed'
+			      }
+			      res.jsonp(responsedata);
+			    }
+  			});
+          	
+      	}
+	  });
 }
