990960e28d8d15e863f2ddb8f2b491decf248fa6
sameervedpathak <sameer.vedpathak18@gmail.com>
2016-01-12 18:11:37 +0530

    testing query in sendnotification

--- templates/api/sendpushnotification.js
+++ templates/api/sendpushnotification.js
@@ -12,6 +12,7 @@ var deviceCrud  = CRUD(connection,'device_information');
 exports.sendnotification = function(req,res){
 	console.log(req.body);
 	var query1 = "SELECT todo_id,todo_data,user_id,reminder_date,reminder_time,deviceid,platform,device_token FROM device_information JOIN todos ON device_information.userid=todos.user_id where userid = req.body.user_id";
+	console.log("query1:",query1);
 	connection.query(query1, function( error , result ){
       if(result ){
         responsedata = {
