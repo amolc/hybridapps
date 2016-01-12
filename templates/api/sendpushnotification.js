925e100c77347b8fc9d40c7229fd4fdf61cfe408
sameervedpathak <sameer.vedpathak18@gmail.com>
2016-01-12 17:58:02 +0530

    cron in process

--- templates/api/sendpushnotification.js
+++ templates/api/sendpushnotification.js
@@ -7,9 +7,25 @@ var connection = env.Dbconnection;
 var SendNotification = CRUD(connection,'todos');
 var deviceCrud  = CRUD(connection,'device_information');
 
+
+
 exports.sendnotification = function(req,res){
 	console.log(req.body);
-	deviceCrud.load({},function(err,val){
-		console.log(val);
-	});
+	var query1 = "SELECT todo_id,todo_data,user_id,reminder_date,reminder_time,deviceid,platform,device_token FROM device_information JOIN todos ON device_information.userid=todos.user_id where userid = req.body.user_id";
+	connection.query(query1, function( error , result ){
+      if(result ){
+        responsedata = {
+              status: true,
+              record : result
+          }
+      } else {
+        responsedata = {
+              status: false,
+              message:'Failed to get data'
+          }
+      }
+      console.log(result)
+      res.jsonp( responsedata );
+    });
+
 };
