27ab23dbba141969470781565ae1f23e583fa385
sameervedpathak <sameer.vedpathak18@gmail.com>
2016-01-12 16:37:48 +0530

    cron job in process

--- templates/api/sendpushnotification.js
+++ templates/api/sendpushnotification.js
@@ -5,8 +5,11 @@ var env = require('./environment');
 //var sendnotification = require('./pushnotification');
 var connection = env.Dbconnection;
 var SendNotification = CRUD(connection,'todos');
-
+var deviceCrud  = CRUD(connection,'device_information');
 
 exports.sendnotification = function(req,res){
 	console.log(req.body);
-}
+	deviceCrud.load({},function(err,val){
+		console.log(val);
+	});
+};
