67c9c6b5359f426c534448d16ffbe78d6bf62cc5
sameervedpathak <sameer.vedpathak18@gmail.com>
2016-01-13 19:29:59 +0530

    sendpushnotification condition change

--- templates/api/sendpushnotification.js
+++ templates/api/sendpushnotification.js
@@ -8,6 +8,9 @@ var deviceCrud  = CRUD(connection,'device_information');
 var exports = module.exports = {};
 var async = require("async");
 
+// var CronJob = require('cron').CronJob;                    
+// new CronJob('* * * * * *', function() {
+
 exports.sendnotification = function(req,res){
             var gcm = require('node-gcm');
             var sender = new gcm.Sender('AIzaSyAJ9kNU7h4VSK2oiqrD5EatNVvzBD6zsxw');
@@ -23,25 +26,61 @@ exports.sendnotification = function(req,res){
             var userid = req.body.user_id;
             var registrationIds = [];
             var remidermessages = [];
+            //var d = new Date();
           
           var query1 = "SELECT todo_id,todo_data,user_id,reminder_date,reminder_time,deviceid,platform,device_token FROM device_information JOIN todos ON device_information.userid=todos.user_id";
             //console.log("query1:",query1);
           connection.query(query1, function( error , result ){
-            
-              for(i=0;i<result.length;i++){
-                  console.log('*****' + result[i].device_token + '*****');
-                  console.log('*****' + result[i].todo_data + '*****');
-                  remidermessages = result[i].todo_data;
-                  registrationIds = result[i].device_token;
-                  message.addData('message', remidermessages);
+              //console.log("line no 33:",d.yyyymmdd());
+              // for(i=0;i<result.length;i++){
+              //     // if(result[i].reminder_date = d.yyyymmdd()){
+              //     //   console.log("Date Match")
+              //     // }
+              //     console.log('*****' + result[i].device_token + '*****');
+              //     console.log('*****' + result[i].todo_data + '*****');
+              //     remidermessages = result[i].todo_data;
+              //     registrationIds = result[i].device_token;
+              //     message.addData('message', remidermessages);
 
-                  sender.send(message, registrationIds, function(err,result1) {
-                      console.log("the result is");
-                      console.log(result1);
-                      console.log( err );
-                  });
+              //     sender.send(message, registrationIds, function(err,result1) {
+              //         console.log("the result is");
+              //         console.log(result1);
+              //         console.log( err );
+              //     });
                   
-              }
+              // }
+              for (var i = 0; i < result.length; i++) {
+                if (result[i].reminder_date.yyyymmdd() == currentdate.yyyymmdd()) {
+                    //console.log("Todays Match Found");
+                    remidermessages = result[i].todo_data;
+                    registrationIds = result[i].device_token;
+                    message.addData('message', remidermessages);
+
+                    sender.send(message, registrationIds, function(err,result1) {
+                        console.log("the result is");
+                        console.log(result1);
+                        console.log( err );
+                    });
+                  };
+
+              };
                 res.jsonp(result);
           });
 };
+
+/***** javascript function for getting date*******/
+
+ Date.prototype.yyyymmdd = function() {
+   var yyyy = this.getFullYear().toString();
+   var mm = (this.getMonth()+1).toString(); 
+   var dd  = this.getDate().toString();
+   return yyyy +"-"+ (mm[1]?mm:"0"+mm[0])+"-"+(dd[1]?dd:"0"+dd[0]);
+  };
+
+currentdate = new Date();
+currentdate.yyyymmdd();
+//console.log(currentdate.yyyymmdd());
+
+/********************* End ******************************/ 
+
+ //}, null, true, 'America/Los_Angeles');
