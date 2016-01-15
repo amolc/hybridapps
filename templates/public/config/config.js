2dd63cd463dd31bb2f0a8aa8e0f17b3eba7e7da2
sameervedpathak <sameer.vedpathak18@gmail.com>
2016-01-15 14:29:07 +0530

    config

--- templates/public/config/config.js
+++ templates/public/config/config.js
@@ -1,10 +1,8 @@
 if (document.location.hostname == "node.fountaintechies.com" ){
-	var baseUrl = "http://node.fountaintechies.com:5555/api/";
+	var baseUrl = "http://localhost:9999/api/";
 
 } else {
 
-	var baseUrl = "http://localhost:5555/api/";
+	var baseUrl = "http://localhost:9999/api/";
 	
 }
-
-
